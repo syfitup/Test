e41f2e04104910f309e74fc9bc5ddb8cb5f3783d
sipho khumalo <sipho.khumalo@pipware.net>
2020-02-19 21:37:28 +0200

    Working axios and department edit

--- WebApplication1/ClientApp/src/admin/departments/Edit.js
+++ WebApplication1/ClientApp/src/admin/departments/Edit.js
@@ -1,27 +1,71 @@
-﻿import React, { useState } from 'react';
+﻿import React, { useState, useEffect } from 'react';
+import { DepartmentClient } from '../../services/data/departmentClient';
+
 
 function EditDepartment() {
 
-    const [name, setName] = useState('John Doe');
-
-    const alertName = () => {
-        alert(name);
+    const department = {
+        name: '',
+        description: '',
+        code: ''
     };
 
-    const handleNameInput = e => {
-        setName(e.target.value);
+    const [userName, setUsername] = useState('JD');
+    const [firstName, setFirstname] = useState('John');
+    const [lastName, setLastname] = useState('Doe');
+    const [model, setModel] = useState(department);
+
+    const departmentClient = new DepartmentClient();
+
+
+    useEffect(() => {
+        departmentClient.getById("7FE7C550-8FB4-E711-B0AE-28F10E00516D").then(response => {
+            setModel(response.data);
+        });
+    });
+
+    const logName = () => {
+        // do whatever with the names ...
+        console.log(this.state.userName);
+        console.log(this.state.firstName);
+        console.log(this.state.lastName);
+    };
+
+    const handleUserNameInput = e => {
+        setUsername({ userName: e.target.value });
+    };
+    const handleFirstNameInput = e => {
+        setFirstname({ firstName: e.target.value });
+    };
+    const handleLastNameInput = e => {
+        setLastname({ lastName: e.target.value });
     };
 
     return (
         <div>
-            <h3> This is a Functional Component </h3>
+            <h3> The text fields will update in 5 seconds </h3>
             <input
                 type="text"
-                onChange={handleNameInput}
-                value={name}
-                placeholder="Your name"
+                onChange={handleUserNameInput}
+                value={model.name}
+                placeholder="Your username"
             />
-            <button onClick={alertName}> Alert </button>
+            <input
+                type="text"
+                onChange={handleFirstNameInput}
+                value={model.description}
+                placeholder="Your firstname"
+            />
+            <input
+                type="text"
+                onChange={handleLastNameInput}
+                value={model.code}
+                placeholder="Your lastname"
+            />
+            <button className="btn btn-large right" onClick={logName}>
+                {' '}
+                Log Names{' '}
+            </button>
         </div>
     );
 };
--- WebApplication1/ClientApp/src/services/data/departmentClient.js
+++ WebApplication1/ClientApp/src/services/data/departmentClient.js
@@ -1,23 +1,23 @@
 ﻿import axios, { AxiosResponse } from 'axios';
 
 export class DepartmentClient {
-    getById(id: string): any {
+    getById(id): any {
         return axios.get(`/api/departments/${id}`);
     }
 
-    search(criteria: any):any {
+    search(criteria):any {
         return axios.get("/api/departments", { params: criteria });
     }
 
-    create(model: any): any{
+    create(model): any{
         return axios.post("/api/departments", model);
     }
 
-    update(id: string, model): any {
+    update(id, model): any {
         return axios.put(`/api/departments/${id}`, model);
     }
 
-    delete(id: string):any {
+    delete(id ):any {
         return axios.delete(`/api/departments/${id}`);
     }
 }
