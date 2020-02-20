24569eadafa9b5baed21ea4417e7779cee6e73f8
sipho khumalo <sipho.khumalo@pipware.net>
2020-02-20 18:39:53 +0200

    Added an editable react hook

--- WebApplication1/ClientApp/src/admin/departments/Edit.js
+++ WebApplication1/ClientApp/src/admin/departments/Edit.js
@@ -1,6 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
 import { DepartmentClient } from '../../services/data/departmentClient';
-
+import TextFieldGroup from '../../common/TextFieldGroup';
 
 function EditDepartment() {
 
@@ -10,62 +10,61 @@ function EditDepartment() {
         code: ''
     };
 
-    const [userName, setUsername] = useState('JD');
-    const [firstName, setFirstname] = useState('John');
-    const [lastName, setLastname] = useState('Doe');
+    const test = "";
     const [model, setModel] = useState(department);
-
     const departmentClient = new DepartmentClient();
-
 
     useEffect(() => {
         departmentClient.getById("7FE7C550-8FB4-E711-B0AE-28F10E00516D").then(response => {
             setModel(response.data);
         });
-    });
+    }, test);
 
-    const logName = () => {
-        // do whatever with the names ...
-        console.log(this.state.userName);
-        console.log(this.state.firstName);
-        console.log(this.state.lastName);
+    const onChange = e => {
+        const updateModel = { ...model };
+        updateModel[e.target.name] = e.target.value;
+        setModel(updateModel);
     };
 
-    const handleUserNameInput = e => {
-        setUsername({ userName: e.target.value });
-    };
-    const handleFirstNameInput = e => {
-        setFirstname({ firstName: e.target.value });
-    };
-    const handleLastNameInput = e => {
-        setLastname({ lastName: e.target.value });
+    const onSubmit = e =>  {
+        e.preventDefault();
     };
 
     return (
-        <div>
-            <h3> The text fields will update in 5 seconds </h3>
-            <input
-                type="text"
-                onChange={handleUserNameInput}
-                value={model.name}
-                placeholder="Your username"
-            />
-            <input
-                type="text"
-                onChange={handleFirstNameInput}
-                value={model.description}
-                placeholder="Your firstname"
-            />
-            <input
-                type="text"
-                onChange={handleLastNameInput}
-                value={model.code}
-                placeholder="Your lastname"
-            />
-            <button className="btn btn-large right" onClick={logName}>
-                {' '}
-                Log Names{' '}
-            </button>
+        <div className="register">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 m-auto">
+                        <h1 className="display-4 text-center">Sign Up</h1>
+                        <p className="lead text-center">
+                            Add a Sub Department
+                            </p>
+                        <form noValidate onSubmit={onSubmit}>
+                            <TextFieldGroup
+                                placeholder="Name"
+                                name="name"
+                                value={model.name}
+                                onChange={onChange}
+                            />
+                            <TextFieldGroup
+                                placeholder="Code"
+                                name="code"
+                                value={model.code}
+                                onChange={onChange}
+                            />
+                            <TextFieldGroup
+                                placeholder="Description"
+                                name="description"
+                                value={model.description}
+                                onChange={onChange}
+                                info="The name to use when logging in!"
+                            />
+
+                            <input type="submit" className="btn btn-info btn-block mt-4" />
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
