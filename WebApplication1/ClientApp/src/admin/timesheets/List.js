5181d17deb966ce698a552d29406892d82fde864
sipho khumalo <sipho.khumalo@pipware.net>
2020-03-03 11:27:37 +0200

    Implements timesheet columns and employee rows to be able to capture time sheets

--- WebApplication1/ClientApp/src/admin/timesheets/List.js
+++ WebApplication1/ClientApp/src/admin/timesheets/List.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect, setState } from 'react';
 import ReactDataGrid from "react-data-grid";
 import { TimesheetsClient } from '../../services/data/timesheetsClient';
 import TextFieldGroup from '../../common/TextFieldGroup';
@@ -12,50 +12,80 @@ function Timesheets() {
         periods: 7
     };
 
-    const columns = [
+    let columns = [
         { key: "id", name: "ID", editable: true },
         { key: "title", name: "Title", editable: true },
         { key: "complete", name: "Complete", editable: true }
     ];
 
-    const rows = [
+    let rows = [
         { id: 0, title: "Task 1", complete: 20 },
         { id: 1, title: "Task 2", complete: 40 },
         { id: 2, title: "Task 3", complete: 60 }
     ];
 
     const test = "";
-    const [model, setModel] = useState(criteria);
     const timesheetsClient = new TimesheetsClient();
+    const [rowss, setModel] = useState(rows);
+    const [columnss, setColmuns] = useState(columns);
 
     useEffect(() => {
             fetch();
     }, test);
 
     const onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-        this.setState(state => {
-            const rows = state.rows.slice();
+            const updatedRows = rowss.slice();
             for (let i = fromRow; i <= toRow; i++) {
-                rows[i] = { ...rows[i], ...updated };
-            }
-            return { rows };
-        });
+                updatedRows[i] = { ...rowss[i], ...updated };
+        }
+
+        setModel(updatedRows);
+
+        return updatedRows;
+
     };
 
     const onChange = e => {
-        const updateModel = { ...model };
+        const updateModel = { ...rowss };
         updateModel[e.target.name] = e.target.value;
         setModel(updateModel);
     };
 
     function fetch() {
         timesheetsClient.search().then(response => {
-            setModel(response.data);
+         
+            let colData = [{ key: "0", name: "" }];
+
+
+            for (var i = 1; i < response.data.length; i++) {
+                colData.push({
+                    key: i.toString(),
+                    name: response.data[i].timesheetDate,
+                    editable: true
+                });
+
+                if (i > 3) {
+                    break;
+                }
+            }
+
+            //Match the exact number of rows of rows otherwise the grid will not load property.
+
+            let rows = [
+                { 0: "Sipho"},
+                { 0: "Khumalo"},
+                { 0: "Weakness"}
+            ];
+
+            setColmuns(colData);
+          
+
+            setModel(rows);
         });
     };
 
     function save() {
-            timesheetsClient.save(model).then(response => {
+        timesheetsClient.save(rowss).then(response => {
                 var test = response;
             });
     };
@@ -68,13 +98,11 @@ function Timesheets() {
     return (
         <div className="App">
             <ReactDataGrid
-                columns={columns}
-                rowGetter={i => rows[i]}
+                columns={columnss}
+                rowGetter={i => rowss[i]}
                 rowsCount={3}
                 enableCellSelect={true}
                 onGridRowsUpdated={onGridRowsUpdated}
-                minHeight={650}
-                minWidth={650}
             />
         </div>
     );
